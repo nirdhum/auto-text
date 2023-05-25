4d128b477a9b827b9a82aca5cff6d295ea09c450
Nirdhum <66917558+nirdhum@users.noreply.github.com>
2023-05-25 13:41:35 +0530

    Added code to create typing effect

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 const textElement = document.getElementById('text')
 const speedElement = document.getElementById('speed')
+const text = 'Nirdhum Narayan'
+let idx = 1
+let speed = 500 / speedElement.value
+
+writeText()
+
+function writeText() {
+    textElement.innerHTML = text.slice(0, idx)
+
+    idx++
+
+    if (idx > text.length) {
+        idx = 1
+    }
+
+    setTimeout(writeText, speed)
+}
+
+speedElement.addEventListener('input', (e) => speed = 500 / e.target.value)
