baeaf2505b8ace22084209367673789c0d1988f1
Nirdhum <66917558+nirdhum@users.noreply.github.com>
2023-05-25 15:13:25 +0530

    Added functionalties to display text input by user in DOM

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const textElement = document.getElementById('text')
 const speedElement = document.getElementById('speed')
-const text = 'Nirdhum Narayan'
+const inputText = document.getElementById('inputText')
+let text = 'Nirdhum Narayan'
 let idx = 1
 let speed = 500 / speedElement.value
 
@@ -17,5 +18,15 @@ function writeText() {
 
     setTimeout(writeText, speed)
 }
+
+inputText.addEventListener('input', function (e) {
+    e.preventDefault()
+
+    if (inputText.value == '') {
+        text = 'Nirdhum Narayan'
+    } else {
+        text = inputText.value
+    }
+})
 
 speedElement.addEventListener('input', (e) => speed = 500 / e.target.value)
